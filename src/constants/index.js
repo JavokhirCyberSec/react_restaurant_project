fbfaf54f9b9c49172f90fce5766e3fd92a360af2
Javokhir Ungorboev <jungarboev@constructor.university>
2024-01-14 16:18:33 +0100

    Laurels

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 import images from './images';
 import meal from '../assets/meal.mp4';
 import data from './data';
+import steak_cut from '../assets/steak_cut.mp4';
 
-export { images, meal, data };
+export { images, meal, data, steak_cut };
